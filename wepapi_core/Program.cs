7c1aa86a21e52aa018d5b292571ca70d115d7246
Debanjan Chakraborty <frontlook.inc@gmail.com>
2019-09-10 17:45:03 +0530

    try

--- wepapi_core/Program.cs
+++ wepapi_core/Program.cs
@@ -14,11 +14,30 @@ namespace webapi_core_rm
     {
         public static void Main(string[] args)
         {
+            /*var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();*/
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        /*var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseIISIntegration()
+            .UseStartup()
+            .Build();
+
+        host.Run();*/
+
+        
     }
 }
